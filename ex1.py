466c2762146e21af3911a926238774f2d8bbc91d
adela888 <56228869+ada-lovelace-code@users.noreply.github.com>
2020-02-06 13:43:52 +0000

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,8 @@
+x = input ("What is your name: ")
+y = input ("What is your age: ")
+z = int (input ("How many times do you want to print the message?: "))
+
+
+m = ("{} is {} years old. \n".format(x, y))
+
+print (z * m)
