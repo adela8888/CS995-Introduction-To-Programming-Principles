7ec90d424a80315636a6732b2bdad08af98ccfe7
adela8888 <57149121+adela8888@users.noreply.github.com>
2020-04-09 17:10:57 +0200

    Delete ex10.py

--- ex10.py
+++
@@ -1,9 +0,0 @@
-x = input("Enter a random number: ")
-y = range (10000000)
-if x %y != 0:
-    print ("Your number is a prime number.")
-else:
-    print ("Your number is not a prime.")
-
-
-# NOT WORKING
