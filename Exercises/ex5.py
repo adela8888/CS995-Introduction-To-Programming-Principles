13707217499822e49eb14b21b6a9426f4ec0e284
adela8888 <57149121+adela8888@users.noreply.github.com>
2020-04-17 10:17:11 +0200

    Update ex5.py

--- Exercises/ex5.py
+++ Exercises/ex5.py
@@ -1,6 +1,9 @@
+"""
+Takes a word and let's you know if it is same from start to end and the other way around
+"""
 x = input("Enter word: ")
 x = str (x)
 y = x [::-1]
 
-if x == y: print (x)
+if x == y: print ("The word " + x + " is read the same from start to end end the other way around")
 else: print ("Not the same from the end to the start")
