64efdf934a8621db5e34a90e43f5af94134d35cb
adela8888 <57149121+adela8888@users.noreply.github.com>
2020-04-17 10:29:50 +0200

    Update ex1.py

--- Exercises/ex1.py
+++ Exercises/ex1.py
@@ -1,3 +1,6 @@
+"""
+Takes three inputs. Your name, your age and 'z' intenger and prints out your name and age z times
+"""
 x = input ("What is your first name: ")
 y = input ("What is your age: ")
 z = int (input ("How many times do you want to print the message?: "))
