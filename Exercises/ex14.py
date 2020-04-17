b8de353e83b9c4b7b7b4ca5b3a6675ef10373533
adela8888 <57149121+adela8888@users.noreply.github.com>
2020-04-17 10:24:48 +0200

    Update ex14.py

--- Exercises/ex14.py
+++ Exercises/ex14.py
@@ -1,3 +1,6 @@
+"""
+Lottery game that uses a dictionary 
+"""
 lottery = {1: "£50", 2: "chocolate", 3: "nothing", 4: "win nothing", 6: "£10", 7:"£10000",
            8: "roses", 9: "£10", 10: "a bottle of red wine"}
 
@@ -5,5 +8,4 @@ number = int (input("Choose a number 1 to 10: "))
 
 if number in lottery.keys(): print ("You have won {}".format(lottery.get(number)))
 
-lottery2 = {1: "£40", 2: "£109", 3: "nothing", 4: "flowers of your choice"}
 
