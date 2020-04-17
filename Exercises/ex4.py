8bbb92f39ea5980d6bf7d4db310b67b3def0cf55
adela8888 <57149121+adela8888@users.noreply.github.com>
2020-04-17 10:18:19 +0200

    Update ex4.py

--- Exercises/ex4.py
+++ Exercises/ex4.py
@@ -1,3 +1,6 @@
+"""
+Compares to lists of numbers and creates a new list of number occuring in both lists
+"""
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
