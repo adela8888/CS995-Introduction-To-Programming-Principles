d6af8d614883b95c904e618f5e8090b2481535ba
adela8888 <57149121+adela8888@users.noreply.github.com>
2020-04-17 10:06:45 +0200

    Update ex6.py

--- Exercises/ex6.py
+++ Exercises/ex6.py
@@ -1,6 +1,10 @@
+"""
+Takes a list of number and creates a new list of odd number from the original list
+"""
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 b=[]
-for x in a:
+# for every element in the list a if the remainder of %2 is 0 it adds the element to the list b 
+for x in a: 
     if x%2 == 0:
         b.append(x)
 
